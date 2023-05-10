eba090e02bdb93b17c958c309701401a5216472f
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-10 23:08:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,12 @@ function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i =0;i<encodedStr.length;i++){
-		decodedArr.push(lookup[encodedStr[i]]);
+		if(lookup[encodedStr[i]]===undefined){
+			decodedArr.push(encodedStr[i]);
+		}else{
+			decodedArr.push(lookup[encodedStr[i]]);
+		}
+		
 	}
   return decodedArr;//return decodedArr
 }
